b8c52b0691c9e3674d222733c2700baaf0d27a09
etapemejame <etapeemblek@yahoo.com>
2019-07-26 21:42:11 -0400

    Update testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -2,6 +2,6 @@
 echo "This is an example of a script"
 echo "That is running in a build step"
 echo "This is a param $PASSED"
-echo "This should trigger the build for me"
+echo "This should trigger the build"
 NUM=$(($FIRSTNUMBER + $SECONDNUMBER))
 echo "$NUM"
