0af4aa6a21ac01e34f244e5f555e1bf080362f54
root <root@etapemejame4c.mylabserver.com>
2020-06-13 03:30:00 +0000

    Changes made on file

--- testScript.sh
+++ testScript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "This is an example of a script"
-echo "That is running in a build step"
+echo "This will build automatically"
+echo "I love this build"
 echo "This is a param $PASSED"
 NUM=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUM"
